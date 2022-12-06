fc0cbaef28859ebb5fc5769795d6390546045c05
Carlos Eduardo <suportebeloj@gmail.com>
2022-12-06 11:39:23 -0300

    fix syntax

--- tests/test_MexcClient.py
+++ tests/test_MexcClient.py
@@ -4,4 +4,4 @@ from MexcClient.main import MexcClient
 def test_exchange_connection_success():
     client = MexcClient("key", "secret")
     response = client.check_connection()
-    assert response == True
+    assert response is True
