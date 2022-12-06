924cdc5f1ffb305b8f73dcbc61c80169119f3e20
Carlos Eduardo <suportebeloj@gmail.com>
2022-12-06 12:35:08 -0300

    package enums

---
+++ MexcClient/Enums/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class EnumKlineInterval(Enum):
+    ONE_MIN = "1m"
+    FIVE_MIN = "5m"
+    FIFTEEN_MIN = "15m"
+    TIRTY_MIN = "30m"
+    SIXTY_MIN = "60m"
+    FOUR_HOUR = "4h"
+    ONE_DAY = "1d"
+    ONE_MONTH = "1M"
